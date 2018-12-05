0607ca07eaaf05474950077322c4bf85c1b87a4e
byzanteen <andreiopreacretu@yahoo.fr>
2018-12-05 20:00:26 +0200

    Initial commit. Set up user registration and roles, automatically assigning a role, a profile model and Create and Read actions for it.

---
+++ NoBook/Controllers/ProfileController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNet.Identity;
+using NoBook.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NoBook.Controllers
+{
+    public class ProfileController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+        //GET: Create profile
+        public ActionResult Create ()
+        {
+            return View();
+        }
+
+        //POST: Create profile
+        [HttpPost]
+        public ActionResult Create (Profile profile)
+        {
+            profile.UserId = User.Identity.GetUserId();
+            try
+            {
+                db.Profile.Add(profile);
+                db.SaveChanges();
+                return RedirectToAction("Show","Profile",profile.ProfileId);
+            } catch (Exception e) {
+                return View();
+            }
+        }
+
+        // GET: Profile
+        public ActionResult Show(int id)
+        {
+            Profile profile = db.Profile.Find(id);
+            return View(profile);
+        }
+
+
+
+    }
+}
---
+++ NoBook/Models/Profile.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace NoBook.Models
+{
+    public class Profile
+    {
+        [Key]
+        public int ProfileId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string UserId { get; set; }
+
+        public virtual ApplicationUser User { get; set; }
+    }
+}
